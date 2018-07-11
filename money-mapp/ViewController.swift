193a3b48a35a0c282a503caf1af074d21cb4901e
aliceywu <aliceywu@umich.edu>
2018-07-10 21:39:41 -0400

    Initial commit. Storyboard design

---
+++ money-mapp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  money-mapp
+//
+//  Created by Alice Wu on 7/10/18.
+//  Copyright © 2018 Alice Wu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
