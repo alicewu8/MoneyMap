28bc948ada596724be65b29fc99570e210580197
aliceywu <aliceywu@umich.edu>
2018-08-05 20:04:13 -0400

    Added PurchaseInfo VC

---
+++ money-mapp/PurchaseInfo.swift
@@ -0,0 +1,16 @@
+//
+//  PurchaseInfo.swift
+//  money-mapp
+//
+//  Created by Alice Wu on 8/5/18.
+//  Copyright © 2018 Alice Wu. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class PurchaseInfo : UIViewController {
+    
+    
+    
+}
