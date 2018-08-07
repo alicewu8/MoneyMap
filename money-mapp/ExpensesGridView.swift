5e5f243bcd815883f6c5e56ecb6b3cafd44a012e
aliceywu <aliceywu@umich.edu>
2018-08-07 17:37:18 -0400

    About to remove collection view from storyboard

---
+++ money-mapp/ExpensesGridView.swift
@@ -0,0 +1,18 @@
+//
+//  PurchasesGridView.swift
+//  money-mapp
+//
+//  Created by Alice Wu on 8/7/18.
+//  Copyright © 2018 Alice Wu. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ExpensesGridView : UIView {
+    
+    @IBOutlet weak var purchases_collection_view: UICollectionView!
+    
+    
+    
+}
---
+++ money-mapp/ExpensesListView.swift
@@ -0,0 +1,15 @@
+//
+//  ExpensesListViewController.swift
+//  money-mapp
+//
+//  Created by Alice Wu on 8/7/18.
+//  Copyright © 2018 Alice Wu. All rights reserved.
+//
+
+import UIKit
+
+class ExpensesListView: UIView {
+    
+    @IBOutlet weak var purchases_table_view: UITableView!
+    
+}
