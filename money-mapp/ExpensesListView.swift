2a10f848b65ee40b87f2590ed6ffa5a686abcb28
aliceywu <aliceywu@umich.edu>
2018-08-07 20:16:10 -0400

    Adding custom table view cell for purchase

--- money-mapp/ExpensesListView.swift
+++ money-mapp/ExpensesListView.swift
@@ -8,8 +8,43 @@
 
 import UIKit
 
-class ExpensesListView: UIView {
+class ExpensesListView: UIView, UITableViewDelegate, UITableViewDataSource {
     
     @IBOutlet weak var purchases_table_view: UITableView!
+    
+    var parent : ExpensesVC!
+    var category_index : Int!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    
+    override init(frame: CGRect) { // for using CustomView in code
+        super.init(frame: frame)
+    }
+    
+    required init?(coder aDecoder: NSCoder) { // for using CustomView in IB
+        super.init(coder: aDecoder)
+    }
+    
+    func initialize(_ parent: ExpensesVC) {
+        self.parent = parent
+        self.category_index = parent.category_index
+        
+        purchases_table_view.layer.backgroundColor = Canvas.super_light_gray.cgColor
+        
+        purchases_table_view.delegate = self
+        purchases_table_view.dataSource = self 
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
+    
     
 }
---
+++ money-mapp/PurchaseTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  PurchaseTableViewCell.swift
+//  money-mapp
+//
+//  Created by Alice Wu on 8/7/18.
+//  Copyright © 2018 Alice Wu. All rights reserved.
+//
+
+import UIKit
+
+class PurchaseTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
